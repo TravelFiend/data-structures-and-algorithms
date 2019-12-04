cf14de25b4f1cd863e965c527d1467a32f222f52
TravelFiend <mikegrace274@gmail.com>
2019-12-03 20:47:32 -0800

    tests in place

--- challenges/arrayReverse/__tests__/array-reverse.test.js
+++ challenges/arrayReverse/__tests__/array-reverse.test.js
@@ -1,12 +1,10 @@
 const arrayReverse = require('../array-reverse');
 
-describe('code challenges', () => {
-    describe('array reverse', () => {
-        it('should reverse any given array', () => {
-            expect(arrayReverse([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
-            expect(arrayReverse(['a', 's', 'd', 'f', 'g'])).toEqual(['g', 'f', 'd', 's', 'a']);
-            expect(arrayReverse([])).toEqual([]);
-            expect(arrayReverse([21, -43, 45, -34, 23])).toEqual([23, -34, 45, -43, 21]);
-        });
+describe('array reverse', () => {
+    it('should reverse any given array', () => {
+        expect(arrayReverse([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
+        expect(arrayReverse(['a', 's', 'd', 'f', 'g'])).toEqual(['g', 'f', 'd', 's', 'a']);
+        expect(arrayReverse([])).toEqual([]);
+        expect(arrayReverse([21, -43, 45, -34, 23])).toEqual([23, -34, 45, -43, 21]);
     });
 });
