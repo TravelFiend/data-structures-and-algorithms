cdbc6fd99f0608744a06962400c23cfac28f6c97
TravelFiend <mikegrace274@gmail.com>
2019-12-11 00:17:43 -0800

    travis hopefully passing

--- challenges/linkedList/__tests__/linked-list.test.js
+++ challenges/linkedList/__tests__/linked-list.test.js
@@ -7,16 +7,4 @@ describe('Linked List', () => {
             expect(list).toEqual({ 'head': null });
         });
     });
-
-    // describe('includes', () => {
-    //     it('should return true if search value is found and false if not', () => {
-
-    //     });
-    // });
-
-    // describe('toString', () => {
-    //     it('should return a string with all node values', () => {
-
-    //     });
-    // });
 });
