f35ad3dbc3ec9d90486314e6fac4fcd2a7cd94d1
TravelFiend <mikegrace274@gmail.com>
2019-12-29 18:24:51 -0800

    function is now functional

--- challenges/ll-merge/ll-merge.js
+++ challenges/ll-merge/ll-merge.js
@@ -1,105 +1,29 @@
-class Node {
-    constructor(value, next = null) {
-        this.value = value;
-        this.next = next;
-    }
-}
+const { LinkedList } = require('../linkedList/linked-list');
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+const mergeLists = (ll1, ll2) => {
+    let ll3 = new LinkedList();
+    let current1 = ll1.head;
+    let current2 = ll2.head;
 
-    insert(val) {
-        const node = new Node(val, this.head);
-        this.head = node;
-    }
+    ll3.head = current1;
+    let current3 = ll3.head;
 
-    includes(val) {
-        let current = this.head;
-        while(current) {
-            if(current.value === val) {
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-
-    toString() {
-        let str = '';
-        let current = this.head;
-        while(current) {
-            str += `${current.value}, `;
-            current = current.next;
-        }
-        return str;
-    }
-
-    append(val) {
-        let current = this.head;
-        const node = new Node(val);
-        while(current.next) {
-            current = current.next;
-        }
-        current.next = node;
-    }
-
-    insertBefore(val, newVal) {
-        let current = this.head;
-        const node = new Node(newVal);
-        while(current) {
-            if(current.next.value === val) {
-                node.next = current.next;
-                current.next = node;
-            }
-            current = current.next;
+    while(current2){
+        current3.next = current2;
+        current3 = current2;
+        current2 = current2.next;
+        if(current1){
+            current3.next = current1;
+            current3 = current1;
+            current1 = current1.next;
         }
     }
-
-    insertAfter(val, newVal) {
-        let current = this.head;
-        const node = new Node(newVal);
-        while(current) {
-            if(current.value === val) {
-                node.next = current.next;
-                current.next = node;
-            }
-            current = current.next;
-        }
-    }
-
-    kthFromEnd(k) {
-        let current = this.head;
-        let totalNodes = 0;
-        while(current) {
-            totalNodes++;
-            current = current.next;
-        }
-        const place = totalNodes - k;
-        let num = 1;
-        current = this.head;
-        while(current) {
-            if(num !== place) {
-                num++;
-                current = current.next;
-            } else {
-                return current.value;
-            }
-        }
-    }
-}
-
-const mergeLists = (linkedList1, linkedList2) => {
-    let ll1current = linkedList1.head;
-    let ll2current = linkedList2.head;
-    let ll2next = linkedList2.head.next;
-    while(ll1current && ll2current){
-        ll2current.next = ll1current.next;
-        ll1current.next = ll2current;
-        ll1current = ll2current.next;
-        ll2current = ll2next;
+    
+    while(current1){
+        current3.next = current1;
+        current3 = current1;
+        current1 = current1.next;
     }
 };
 
-module.exports = { LinkedList, mergeLists };
+module.exports = { mergeLists };
